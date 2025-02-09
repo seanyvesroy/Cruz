5eb51f73905cd72e755592e509e8cfd775c36f5e
Sean McCarthy <spmccarthy4@gmail.com>
2025-02-08 20:47:21 -0800

    NAVVVV

--- script.js
+++ script.js
@@ -7,3 +7,15 @@
       });
     });
   });
+// Function to toggle the navbar visibility
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
+
+
+
