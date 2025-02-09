a9be38d1ee7f23714fe79d871a78a56a0bca2d86
Sean McCarthy <spmccarthy4@gmail.com>
2025-02-08 19:30:44 -0800

    nav

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
       document.querySelector(this.getAttribute('href')).scrollIntoView({
