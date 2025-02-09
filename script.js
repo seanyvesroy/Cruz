cd3541a509b99e9ce216fb1117027c28ad44930e
Sean McCarthy <spmccarthy4@gmail.com>
2025-02-08 21:14:42 -0800

    I did javascript

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+// Function to toggle the navbar visibility
+const hamburger = document.querySelector(".hamburger");
+const menubar = document.querySelector(".menubar");
+const menuItems = document.querySelector(".menubar ul li a")
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
       document.querySelector(this.getAttribute('href')).scrollIntoView({
         behavior: 'smooth',
         block: 'start' // Ensures the section is aligned at the top
       });
+      menubar.classList.remove("active"); // Remove active class to close the menubar
+      hamburger.classList.remove("hamburger-active"); 
     });
   });
-// Function to toggle the navbar visibility
-const mobileNav = document.querySelector(".hamburger");
-const navbar = document.querySelector(".menubar");
-
-const toggleNav = () => {
-  navbar.classList.toggle("active");
-  mobileNav.classList.toggle("hamburger-active");
-};
-mobileNav.addEventListener("click", () => toggleNav());
+// Toggle the active class to show the menubar
+hamburger.addEventListener("click", () => {
+  menubar.classList.toggle("active");
+  hamburger.classList.toggle("hamburger-active");
+  const scrollPosition = window.scrollY;
+  menubar.style.top = `${scrollPosition}px`;
+});
 
 
 
